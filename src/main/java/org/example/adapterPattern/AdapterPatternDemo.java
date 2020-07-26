47bfbe88f4471be8e2fcba5809572ec9d8ad54fd
Shivam Singal <shivamsingal0666@gmail.com>
2020-07-26 22:32:35 +0530

    final commit

---
+++ src/main/java/org/example/adapterPattern/AdapterPatternDemo.java
@@ -0,0 +1,1 @@
+https://github.com/daggerdaze/ShivamSingal-epam-DesignPatterns.git
---
+++ src/main/java/org/example/iterator/Container.java
@@ -0,0 +1,7 @@
+package org.example.iterator;
+
+import java.util.Iterator;
+
+public interface Container {
+    public Iterator getIterator();
+}
